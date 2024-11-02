a48163fdbb32e57c21792c8a6e240fcde7e2efad
Guilherme <guilhermestelmach@hotmail.com>
2024-11-02 14:24:56 -0300

    swiper na lp da pasta pratic-landing-page inserido

---
+++ module02/tasks/chalangers/pratic-lading-page/assets/scripts/swiper.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+    },
+});
