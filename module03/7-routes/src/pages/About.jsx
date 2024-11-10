28c7b0af26234c116a6fc30445cc11a47c7baf40
Guilherme <guilhermestelmach@hotmail.com>
2024-11-10 14:18:23 -0300

    aula 50 react routes

---
+++ module03/7-routes/src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return(
+        <div className="page">
+            <h1>About Page</h1>
+            <p>This is an example</p>
+        </div>
+    )
+}
+
+export default About
